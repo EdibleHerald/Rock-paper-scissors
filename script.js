71402fe36a6e5fbe65652649c7ba3484f2759a88
EdibleHerald <142949302+EdibleHerald@users.noreply.github.com>
2023-09-28 20:57:25 -0400

    Added if else conditional to check if player won or lost in function playRound

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
+let computerSelection;
+
 function getComputerChoice(){
 let randomChoice = Math.floor(Math.random()*3+1);
-let Choice=(parseInt(randomChoice)==1) ? 'Rock'
-: (randomChoice==2) ? 'Paper' 
-: (randomChoice==3) ? 'Scissors' 
+let Choice=(parseInt(randomChoice)==1) ? 'rock'
+: (randomChoice==2) ? 'paper' 
+: (randomChoice==3) ? 'scissors' 
 :  'Error';
+
 return Choice;
 }
     
-let computerSelection = getComputerChoice();
-let playerSelection = prompt('Rock, paper, or scissors?','')
+let playerSelection;
+
+function playRound(playerSelection,computerSelection){
+computerSelection=getComputerChoice();
+playerSelection = prompt('Rock, paper, or scissors?','').toLowerCase();
+    if (playerSelection=='rock' && computerSelection=='scissors'){
+        alert('You win!')
+    }
+    else if (playerSelection=='rock' && computerSelection=='paper'){
+        alert('You Lose!')
+    }
+    else if (playerSelection=='rock' && computerSelection=='rock'){
+        alert('Tie!')
+    }
+    else if (playerSelection=='paper' && computerSelection=='scissors'){
+        alert('You Lose!')
+    }
+    else if (playerSelection=='paper' && computerSelection=='rock'){
+        alert('You Win!')
+    }
+    else if (playerSelection=='paper' && computerSelection=='paper'){
+        alert('Tie!')
+    }
+    else if (playerSelection=='scissors' && computerSelection=='paper'){
+        alert('You win!')
+    }
+    else if (playerSelection=='scissors' && computerSelection=='rock'){
+        alert('You lose!')
+    }
+    else if (playerSelection=='scissors' && computerSelection=='scissors'){
+        alert('Tie!')
+    }
+    
+}
